968875583dd31d268e09baef6b93d41af06e1c02
Wiktoria <wiktoriabanach@op.pl>
2024-11-19 19:45:22 +0100

    Practice sorting values and adding columns

--- Day-71-Data-Exploration-Pandas/main.py
+++ Day-71-Data-Exploration-Pandas/main.py
@@ -36,3 +36,19 @@ print(clean_df["Starting Median Salary"].min())
 
 # Find which college major has the lowest mid-career salary and how much can people expect to earn through years
 print(clean_df.loc[clean_df["Mid-Career Median Salary"].idxmin()])
+
+# Count difference between two columns and present results as new column in df
+spread_col = clean_df["Mid-Career 90th Percentile Salary"] - clean_df["Mid-Career 10th Percentile Salary"]
+clean_df.insert(1, 'Spread', spread_col)
+
+# Sorting by the lowest spread
+low_risk = clean_df.sort_values('Spread')
+print(low_risk[['Undergraduate Major', 'Spread']].head())
+
+# Sorting by highest spread
+high_risk = clean_df.sort_values('Spread', ascending = False)
+print(high_risk[['Undergraduate Major', 'Spread']].head())
+
+# Sorting by highest values in the 90th percentile
+high_potential = clean_df.sort_values("Mid-Career 90th Percentile Salary", ascending = False)
+print(high_potential[['Undergraduate Major', 'Mid-Career 90th Percentile Salary']].head())
