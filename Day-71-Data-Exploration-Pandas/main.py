e3b8db6642502088dab069fd093e9191ed53f7a4
Wiktoria <wiktoriabanach@op.pl>
2024-11-19 19:31:59 +0100

    Practice accessing columns and cells in dataframe

--- Day-71-Data-Exploration-Pandas/main.py
+++ Day-71-Data-Exploration-Pandas/main.py
@@ -18,3 +18,21 @@ print(df.tail())
 # Delete row with blank values
 clean_df = df.dropna()
 clean_df.tail()
+
+# Find the highest starting salary
+print(clean_df["Starting Median Salary"].max())
+
+# Find which college major earn that much
+print(clean_df["Starting Median Salary"].idxmax())
+print(clean_df["Undergraduate Major"].loc[43])
+
+# Find what college major has the highest mid-career salary and how much they earn
+print(clean_df["Undergraduate Major"].loc[clean_df["Mid-Career Median Salary"].idxmax()])
+print(clean_df["Mid-Career Median Salary"].max())
+
+# Find which college major has the lowest starting salary and how much do graduates earn
+print(clean_df["Undergraduate Major"].loc[clean_df["Starting Median Salary"].idxmin()])
+print(clean_df["Starting Median Salary"].min())
+
+# Find which college major has the lowest mid-career salary and how much can people expect to earn through years
+print(clean_df.loc[clean_df["Mid-Career Median Salary"].idxmin()])
