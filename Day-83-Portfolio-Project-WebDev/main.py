3c83067eaf4ad892a5c67eab25afde2cc81b67d3
Wiktoria <wiktoriabanach@op.pl>
2024-12-03 19:20:49 +0100

    Create database to store project info
    So all informations will be stored there not in script file as till now

--- Day-83-Portfolio-Project-WebDev/main.py
+++ Day-83-Portfolio-Project-WebDev/main.py
@@ -3,6 +3,9 @@ from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
 import os
 from dotenv import load_dotenv
 
@@ -11,6 +14,20 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("APP_KEY")
 Bootstrap5(app)
+
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class Project(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    topic: Mapped[str] = mapped_column(String, nullable=False)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+    image: Mapped[str] = mapped_column(String, nullable=False)
+    github: Mapped[str] = mapped_column(String, nullable=False)
 
 class EmailForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
