d9828075160189cb24f300b0aa5895194ec37fba
Wiktoria <wiktoriabanach@op.pl>
2024-12-11 17:06:03 +0100

    Add possibility to play with computer
    Add possibility to choose game mode - solo or duo
    Add simple computer player to play with
    Computer player randomly choose place on the board from squares_left list

--- Day-84-Portfolio-Project-Python-Scripting/main.py
+++ Day-84-Portfolio-Project-Python-Scripting/main.py
@@ -25,19 +25,38 @@ winning_combinations = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1
 o_player_squares = []
 x_player_squares = []
 
+def mode_choice():
+    while True:
+        mode = input("Would you like to: 1. play with computer or 2. play with friend? (1/2): ")
+        if mode == "1":
+            return "solo"
+        elif mode == "2":
+            return "duo"
+        else:
+            print("Please use 1 or 2 only to choose game mode")
+
 def turn(player_name, player_shape):
     print(f"{player_name} turn ({player_shape})")
-    while True:
-        choice = int(input(f"Choose a square ({", ".join([str(x) for x in squares_left])}): "))
-        if choice in squares_left:
-            squares_left.remove(choice)
-            if player_shape == "o":
-                o_player_squares.append(choice)
-            else:
-                x_player_squares.append(choice)
-            return choice
-        else:
-            print("This square is already occupated. Choose another one.")
+    if player_name == "Computer":
+        choice = random.choice(squares_left)
+    else:
+        while True:
+            try:
+                choice = int(input(f"Choose a square ({", ".join([str(x) for x in squares_left])}): "))
+                if choice in squares_left:
+                    break
+                elif choice not in squares_left:
+                    print("This square is already occupated. Please choose anotcher one.")
+                elif choice > 9:
+                    print("This number isnt even on our board. Please choose anotcher one.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+    squares_left.remove(choice)
+    if player_shape == "o":
+        o_player_squares.append(choice)
+    else:
+        x_player_squares.append(choice)
+    return choice
 
 def board_update(square_num, player_shape):
     board_squares[square_num] = player_shape
@@ -51,25 +70,31 @@ def board_update(square_num, player_shape):
     return print(board)
     
 def check_game_process(o_player_name, x_player_name, player_shape):
-    if squares_left == []:
+    if player_shape == "o":
+        for combination in winning_combinations:
+            if all(square in o_player_squares for square in combination):
+                print(f"{o_player_name} won!")
+                return True
+    elif player_shape == "x":
+        for combination in winning_combinations:
+            if all(square in x_player_squares for square in combination):
+                print(f"{x_player_name} won!")
+                return True
+    elif squares_left == []:
         print("Its a tie!")
         return True
-    else:
-        if player_shape == "o":
-            for combination in winning_combinations:
-                if all(square in o_player_squares for square in combination):
-                    print(f"{o_player_name} won!")
-                    return True
-        else:
-            for combination in winning_combinations:
-                if all(square in x_player_squares for square in combination):
-                    print(f"{x_player_name} won!")
-                    return True
+        
+
 
 print("Welcome in tic tac toe game!\n")
-print("Enter players names: ")
-player_one_name = input("Player one: ")
-player_two_name = input("Player two: ")
+mode_choice = mode_choice()
+if mode_choice == "solo":
+    player_one_name = input("Enter player name: ")
+    player_two_name = "Computer"
+else:
+    print("Enter players names: ")
+    player_one_name = input("Player one: ")
+    player_two_name = input("Player two: ")
 print("---------------------------------------------------------------")
 print("Now we will draw who will start the game!")
 
