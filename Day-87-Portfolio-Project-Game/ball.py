4e35d72027fc6ce0aefa3906346311f05bde147f
Wiktoria <wiktoriabanach@op.pl>
2024-12-18 21:50:33 +0100

    Create ball class and make it bounce from paddle

---
+++ Day-87-Portfolio-Project-Game/ball.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.goto(0, -180)
+        self.x_move = 1
+        self.y_move = 1
+        self.move_speed = 0.1
+        
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() - self.y_move
+        self.goto(new_x, new_y)
+
+    def y_bounce(self):
+        self.y_move *= -1
--- Day-87-Portfolio-Project-Game/main.py
+++ Day-87-Portfolio-Project-Game/main.py
@@ -1,6 +1,19 @@
 from turtle import Screen
 from paddle import Paddle
-from bricks import Bricks
+from ball import Ball
+from brick import Brick
+
+bricks = []
+
+def create_bricks():
+    for row in range(6):
+        for col in range(9):
+            brick = Brick()
+            x = -360 + col * 90
+            y = 280 - row * 50
+            brick.goto(x, y)
+            bricks.append(brick)
+
 
 screen = Screen()
 screen.title("Brakeout Game")
@@ -10,13 +23,21 @@ screen.tracer(0)
 
 paddle = Paddle()
 
-brick = Bricks()
-brick.bricks_setup()
-screen.update()
+ball = Ball()
 
-screen.tracer(1)
+create_bricks()
+
 screen.listen()
 screen.onkeypress(paddle.go_left, "Left")
 screen.onkeypress(paddle.go_right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    ball.move()
+    if ball.ycor() > -240 and ball.ycor() < -230 and ball.distance(paddle) < 60:
+        ball.y_bounce()
+    
+                        
 
 screen.mainloop()
