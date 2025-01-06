64c3065c698d0e94bacdd3e48076006b9bc72add
Wiktoria <wiktoriabanach@op.pl>
2025-01-06 18:38:48 +0100

    Make root window to run in the background
    When window is hidden filedialog in choose_save_path doesnt pop up

--- Day-91-Portfolio-Project-Requests&APIs/main.py
+++ Day-91-Portfolio-Project-Requests&APIs/main.py
@@ -1,8 +1,9 @@
 import tkinter
-from helper_functions import select_file, pdf_to_text, text_to_speech, save_file
+from helper_functions import select_file, pdf_to_text, text_to_speech, choose_save_path, save_file
 
 root = tkinter.Tk()
-root.withdraw()
+root.attributes('-topmost', True) 
+root.iconify()
 
 print("Welcome in tts converter!")
 print("Choose file to convert: ")
@@ -16,4 +17,7 @@ if file_path:
         print(text)
     response = text_to_speech(text = text)
     title = input("Enter new file name: ")
-    save_file(title, response)
+    save_file_path = choose_save_path(title)
+    if save_file_path:
+        save_file(save_file_path, response)
+root.destroy()
